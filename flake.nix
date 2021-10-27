a665ef0095fb3a5722d49d5dfcdd205dbff4a4d9
Shea Levy <shea@shealevy.com>
2021-10-27 19:28:07 -0400

    Repo is marlowe-website only

--- flake.nix
+++ flake.nix
@@ -1,97 +1,40 @@
-# NOTE: This flake is only provided as interface to `bitte` and shouldn't be used otherwise
-#
-# Occasionally building flake builds will segfault. The workaround for this is to
-# disable the garbage collector  `GC_DONT_GC=1  nix build .#web-ghc-server
-#
-# In case you are not sure if you should be using this flake, the answer is: No.
 {
-  description = "plutus flake for pinning sources and bitte deployments";
+  description = "The Marlowe website";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
 
-    # We intentionally import nixpkgs and haskell.nix as non-flakes, to match the
-    # flake-free normal build workflow exactly.
-    nixpkgs = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-      ref = "nixpkgs-unstable";
-      flake = false;
-    };
-    haskell-nix = {
-      url = "github:input-output-hk/haskell.nix";
-      flake = false;
-    };
+    nixpkgs.url= "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
 
-    actus-tests = {
-      url = "github:actusfrf/actus-tests";
-      flake = false;
-    };
-    cardano-repo-tool = {
-      url = "github:input-output-hk/cardano-repo-tool";
-      flake = false;
-    };
-    easy-purescript-nix = {
-      url = "github:justinwoo/easy-purescript-nix";
-      flake = false;
-    };
-    gitignore-nix = {
-      url = "github:hercules-ci/gitignore.nix";
-      flake = false;
-    };
-    hackage-nix = {
-      url = "github:input-output-hk/hackage.nix";
-      flake = false;
-    };
-    haskell-language-server = {
-      # Pinned to a release
-      url = "github:haskell/haskell-language-server?ref=1.3.0";
-      flake = false;
-    };
-    iohk-nix = {
-      url = "github:input-output-hk/iohk-nix";
-      flake = false;
-    };
+    gitignore-nix.url = "github:hercules-ci/gitignore.nix";
+
     npmlock2nix = {
       url = "github:tweag/npmlock2nix";
       flake = false;
     };
-    plutus-core = {
-      url = "github:input-output-hk/plutus";
-      flake = false;
-    };
-    plutus-apps = {
-      url = "github:input-output-hk/plutus-apps";
-      flake = false;
-    };
-    pre-commit-hooks-nix = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      flake = false;
-    };
-    spago2nix = {
-      url = "github:justinwoo/spago2nix";
-      flake = false;
-    };
-    sphinxcontrib-haddock = {
-      url = "github:michaelpj/sphinxcontrib-haddock";
-      flake = false;
-    };
-    stackage-nix = {
-      url = "github:input-output-hk/stackage.nix";
-      flake = false;
-    };
   };
 
-  outputs = { self, flake-utils, ... }@inputs:
-    (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        topLevel = import ./. {
-          inherit system;
-          sources = inputs;
+  outputs = { self, flake-utils, nixpkgs, npmlock2nix, gitignore-nix }:
+  flake-utils.lib.simpleFlake {
+    inherit self nixpkgs;
+
+    name = "marlowe-website";
+
+    preOverlays = [ gitignore-nix.overlay ];
+
+    overlay = final: prev: let
+      npmlock2nix-build = (final.callPackage npmlock2nix { }).build;
+    in {
+      marlowe-website.defaultPackage = npmlock2nix-build {
+        src = final.gitignoreSource ./.;
+        installPhase = "cp -r public $out";
+        node_modules_mode = "copy";
+
+        node_modules_attrs = {
+          packageLockJson = ./package-lock.json;
+          packageJson = ./package.json;
         };
-      in
-      {
-        packages = topLevel.bitte-packages;
-      }));
+      };
+    };
+  };
 }
